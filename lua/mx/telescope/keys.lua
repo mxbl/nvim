3d0f976c011c2c8ea40abb6e33ed2e4d2c0ff598
Maximilian Block <max@pool.math.tu-berlin.de>
2023-10-12 09:43:09 +0200

    some changes

--- lua/mx/telescope/keys.lua
+++ lua/mx/telescope/keys.lua
@@ -1,7 +1,7 @@
 local mx_tele = require 'mx.telescope'
 local builtin = require 'telescope.builtin'
 
-vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
+-- vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>pf', mx_tele.find_files, {})
 vim.keymap.set('n', '<leader>pc', mx_tele.edit_neovim, {})
 vim.keymap.set('n', '<leader>pe', mx_tele.file_browser, {})
