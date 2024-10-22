e8cd4355cf4c1a8bc29595ae83335a22ea74b7d0
Maximilian Block <max@pool.math.tu-berlin.de>
2024-10-22 21:08:15 +0200

    Switch from lir to oil

--- lua/custom/plugins/lir.lua
+++ lua/custom/plugins/lir.lua
@@ -1,59 +1,59 @@
 return {
-  {
-    'tamago324/lir.nvim',
-    dependencies = {
-      'tamago324/lir-git-status.nvim',
-      'kyazdani42/nvim-web-devicons',
-    },
-
-    config = function() end,
-
-    config = function()
-      require'nvim-web-devicons'.set_icon({
-        lir_folder_icon = {
-          icon = "",
-          color = "#7ebae4",
-          name = "LirFolderNode",
-        },
-      })
-      require('lir').setup {
-        show_hidden_files = true,
-        devicons = { enable = true },
-        float = {
-          winblend = 15,
-          curdir_window = {
-            enable = false,
-            highlight_dirname = true,
-          },
-          win_opts = function()
-            local width = math.floor(vim.o.columns * 0.8)
-            return {
-              border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-              width = width,
-              height = math.floor(vim.o.lines * 0.8),
-              row = 1,
-              col = math.floor((vim.o.columns - width) / 2),
-            }
-          end,
-        },
-        hide_cursor = true,
-        mappings = {
-          ['<cr>'] = require'lir.actions'.edit,
-          ['-']    = require'lir.actions'.up,
-          ['q']    = require'lir.actions'.quit,
-          ['@']    = require'lir.actions'.cd,
-          ['M']    = require'lir.actions'.mkdir,
-          ['N']    = require'lir.actions'.newfile,
-          ['R']    = require'lir.actions'.rename,
-          ['D']    = require'lir.actions'.delete,
-        },
-      }
-
-      require('lir.git_status').setup {
-        show_ignored = false,
-      }
-
-      vim.keymap.set('n', '<leader>pv', function() require'lir.float'.toggle() end)
-    end,
-  },
+	-- {
+	-- 	"tamago324/lir.nvim",
+	-- 	dependencies = {
+	-- 		"tamago324/lir-git-status.nvim",
+	-- 		"kyazdani42/nvim-web-devicons",
+	-- 	},
+	--
+	-- 	config = function()
+	-- 		require("nvim-web-devicons").set_icon({
+	-- 			lir_folder_icon = {
+	-- 				icon = "",
+	-- 				color = "#7ebae4",
+	-- 				name = "LirFolderNode",
+	-- 			},
+	-- 		})
+	-- 		require("lir").setup({
+	-- 			show_hidden_files = true,
+	-- 			devicons = { enable = true },
+	-- 			float = {
+	-- 				winblend = 15,
+	-- 				curdir_window = {
+	-- 					enable = false,
+	-- 					highlight_dirname = true,
+	-- 				},
+	-- 				win_opts = function()
+	-- 					local width = math.floor(vim.o.columns * 0.8)
+	-- 					return {
+	-- 						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	-- 						width = width,
+	-- 						height = math.floor(vim.o.lines * 0.8),
+	-- 						row = 1,
+	-- 						col = math.floor((vim.o.columns - width) / 2),
+	-- 					}
+	-- 				end,
+	-- 			},
+	-- 			hide_cursor = true,
+	-- 			mappings = {
+	-- 				["<cr>"] = require("lir.actions").edit,
+	-- 				["-"] = require("lir.actions").up,
+	-- 				["q"] = require("lir.actions").quit,
+	-- 				["@"] = require("lir.actions").cd,
+	-- 				["M"] = require("lir.actions").mkdir,
+	-- 				["N"] = require("lir.actions").newfile,
+	-- 				["R"] = require("lir.actions").rename,
+	-- 				["D"] = require("lir.actions").delete,
+	-- 			},
+	-- 		})
+	--
+	-- 		require("lir.git_status").setup({
+	-- 			show_ignored = false,
+	-- 		})
+	--
+	-- 		-- vim.keymap.set("n", "<leader>pv", function()
+	-- 		-- 	require("lir.float").toggle()
+	-- 		-- end)
+	-- 	end,
+	-- },
 }
---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			CustomOilBar = function()
+				local path = vim.fn.expand("%")
+				path = path:gsub("oil://", "")
+				return " " .. vim.fn.fnamemodify(path, ":.")
+			end
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<c-h>"] = false,
+					["<c-l>"] = false,
+					["<c-k>"] = false,
+					["<c-j>"] = false,
+					["q"] = "actions.close",
+					["@"] = "actions.cd",
+				},
+				-- win_options = {
+				-- 	winbar = "%{v:lua.CustomOilBar()}",
+				-- },
+				view_options = {
+					show_hidden = true,
+				},
+			})
+			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<leader>pv", require("oil").toggle_float)
+		end,
+	},
+}
