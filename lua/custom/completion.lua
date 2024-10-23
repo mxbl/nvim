021e73681ec622809222f6122ad589728f85e383
Maximilian Block <max@pool.math.tu-berlin.de>
2024-10-23 08:44:10 +0200

    Luasnip working?

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -1,35 +1,46 @@
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.shortmess:append 'c'
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.shortmess:append("c")
 
-local lspkind = require'lspkind'
-lspkind.init {}
+local lspkind = require("lspkind")
+lspkind.init({})
 
-local cmp = require'cmp'
+local cmp = require("cmp")
 
-cmp.setup {
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'buffer' },
-  },
-  mapping = {
-    ['<c-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ['<c-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ['<c-y>'] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { 'i', 'c' }
-    ),
-  },
+local ls = require("luasnip")
 
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-}
+vim.snippet.expand = ls.lsp_expand
+vim.snippet.stop = ls.unlink_current
+
+ls.config.set_config({
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+	override_builtin = true,
+})
+
+cmp.setup({
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "buffer" },
+	},
+	mapping = {
+		["<c-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<c-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<c-y>"] = cmp.mapping(
+			cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = true,
+			}),
+			{ "i", "c" }
+		),
+	},
+
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end,
+	},
+})
 
 -- TODO: vim-dadbod
 -- cmp.setup.filetype({ 'spl' }, {
