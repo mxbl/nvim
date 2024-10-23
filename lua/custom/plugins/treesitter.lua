52a39c489f24c4c4145dc6d483cfe578e11e5438
Maximilian Block <max@pool.math.tu-berlin.de>
2024-10-23 08:43:44 +0200

    lua style format

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    -- branch = 'main',
-    -- lazy = 'false',
-    config = function()
-      local configs = require'nvim-treesitter.configs'
-      configs.setup({
-        ensure_installed = { "lua", "json", "vim", "vimdoc", "go", "yaml" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		-- branch = 'main',
+		-- lazy = 'false',
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "lua", "json", "vim", "vimdoc", "go", "yaml" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
