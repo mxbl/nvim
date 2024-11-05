83b69692ebba27066cc3492e3b71e9e5c8f87eb7
Maximilian Block <max@pool.math.tu-berlin.de>
2024-11-05 11:03:44 +0100

    Setup nvim for terminal usage

---
+++ plugin/terminal.lua
@@ -0,0 +1,33 @@
+local set = vim.opt_local
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", {}),
+	callback = function()
+		set.number = false
+		set.relativenumber = false
+		set.scrolloff = 0
+		vim.bo.filetype = "terminal"
+	end,
+})
+
+vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
+vim.keymap.set("t", "<M-u>", "<c-\\><c-n>gT")
+vim.keymap.set("t", "<M-i>", "<c-\\><c-n>gt")
+
+vim.cmd([[
+	autocmd TermOpen term://* startinsert
+	autocmd BufWinEnter,WinEnter term://* startinsert
+]])
+
+vim.keymap.set("n", ",st", function()
+	vim.cmd.new()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 12)
+	vim.wo.winfixheight = true
+	vim.cmd.term()
+end)
+
+vim.keymap.set("n", ",tt", function()
+	vim.cmd.tabnew()
+	vim.cmd.term()
+end)
