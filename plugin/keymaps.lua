e5ec1309bafd445dc2d057d6491476331dd49be8
Maximilian Block <block@math.tu-berlin.de>
2025-01-27 09:57:13 +0100

    keymaps from vim to lua

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,6 +1,11 @@
 local set = vim.keymap.set
 local k = vim.keycode
 
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+set("n", "<c-n>", "<cmd>cnext<cr>zz")
+set("n", "<c-p>", "<cmd>cprev<cr>zz")
+
 set("n", "<c-j>", function()
 	require("custom.tmux").navigate("j")
 end)
