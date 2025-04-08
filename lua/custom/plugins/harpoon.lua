ea872358ab9e52f6f7b93dfa19df76e948f127b8
Maximilian Block <block@math.tu-berlin.de>
2025-04-08 10:04:58 +0200

    Switch harpoon bindings

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,15 +5,15 @@ return {
 		local harpoon = require("harpoon")
 		harpoon:setup()
 
-		vim.keymap.set("n", ",,a", function()
+		vim.keymap.set("n", ",a", function()
 			harpoon:list():add()
 		end)
 		vim.keymap.set("n", ",e", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		for id, ids in ipairs({ "a", "s", "d", "f", "g" }) do
-			vim.keymap.set("n", string.format(",%s", ids), function()
+		for id, ids in ipairs({ "1", "2", "3", "4", "5" }) do
+			vim.keymap.set("n", string.format("<leader>%s", ids), function()
 				harpoon:list():select(id)
 			end)
 		end
