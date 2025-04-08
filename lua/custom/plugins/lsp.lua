95f0a26d86594060082bdb8a3dda686c03ad9ff7
Maximilian Block <block@math.tu-berlin.de>
2025-04-08 10:13:36 +0200

    some stuff working haha

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -152,7 +152,7 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				go = { "goimports", "goimports-reviser", "gofumpt" },
+				go = { "goimports", "goimports-reviser", "gofumpt", "golines" },
 				nix = { "nixpkgs_fmt" },
 				terraform = { "terraform_fmt" },
 			},
