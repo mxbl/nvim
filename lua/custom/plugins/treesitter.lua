f952499086f9255c50b8e7132155473adbc78a12
Maximilian Block <4mxblc@pm.me>
2025-10-16 11:13:58 +0200

    TS: ensure installed bash

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			local configs = require("nvim-treesitter.configs")
 			configs.setup({
-				ensure_installed = { "lua", "json", "vim", "vimdoc", "go", "yaml" },
+				ensure_installed = { "lua", "json", "vim", "vimdoc", "go", "yaml", "bash" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
